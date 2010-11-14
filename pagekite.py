62cf8ed59f9ba9edcf57fa48b173da1990a6655c
Bjarni R. Einarsson <bre@beanstalks-project.net>
2010-11-14 21:54:08 +0000

    support for WebDAV and other missing HTTP request methods

--- pagekite.py
+++ pagekite.py
@@ -733,7 +733,9 @@ class HttpUiThread(threading.Thread):
     self.httpd.socket.close()
 
 
-HTTP_METHODS = ['OPTIONS', 'GET', 'POST', 'PUT']
+HTTP_METHODS = ['OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'TRACE',
+                'PROPFIND', 'PROPPATCH', 'MKCOL', 'DELETE', 'COPY', 'MOVE',
+                'LOCK', 'UNLOCK' ]
 HTTP_VERSIONS = ['HTTP/1.0', 'HTTP/1.1']
 
 class HttpParser(object):
