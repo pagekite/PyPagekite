c0d02822996b6f9faaab182cc28ba34d52233841
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-01-21 15:55:09 +0000

    Prepping 0.3.12

--- pagekite.py
+++ pagekite.py
@@ -100,7 +100,7 @@
 ###############################################################################
 #
 PROTOVER = '0.8'
-APPVER = '0.3.11+github'
+APPVER = '0.3.12'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 DOC = """\
