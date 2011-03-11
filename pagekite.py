236d40a4aaba1d8c95cdb75374db9355ba0e7680
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-03-11 13:23:51 +0000

    Version bumped to 0.3.16

--- pagekite.py
+++ pagekite.py
@@ -101,7 +101,7 @@
 ###############################################################################
 #
 PROTOVER = '0.8'
-APPVER = '0.3.15+github'
+APPVER = '0.3.16'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 DOC = """\
