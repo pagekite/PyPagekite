bc8928735606d028ce7b021772d8852a741a80e5
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-05-13 21:41:13 +0000

    Prepping v0.4.2

--- pagekite.py
+++ pagekite.py
@@ -101,7 +101,7 @@
 ###############################################################################
 #
 PROTOVER = '0.8'
-APPVER = '0.4.1+github'
+APPVER = '0.4.2'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 LICENSE_URL = 'http://www.gnu.org/licenses/agpl.html'
