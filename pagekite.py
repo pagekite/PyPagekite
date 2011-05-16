257caf38dc6a4d4f67fa71c1c393fd6c0fa76939
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-05-16 16:24:15 +0000

    Moving past 0.4.2

--- pagekite.py
+++ pagekite.py
@@ -101,7 +101,7 @@
 ###############################################################################
 #
 PROTOVER = '0.8'
-APPVER = '0.4.2'
+APPVER = '0.4.2+github'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 LICENSE_URL = 'http://www.gnu.org/licenses/agpl.html'
