12a5a79a3e4c27fd8df437bfe0d8657a53ad0cb6
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-05-22 19:04:47 +0000

    Moving past 0.3.20

--- pagekite.py
+++ pagekite.py
@@ -94,7 +94,7 @@
 ###############################################################################
 #
 PROTOVER = '0.8'
-APPVER = '0.3.20'
+APPVER = '0.3.20+github'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 DOC = """\
