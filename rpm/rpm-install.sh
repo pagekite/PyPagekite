9347abdb45bf610c2e9f9bc8ce0f9c0871a28e34
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-07-31 20:04:32 +0000

    RPM packages get man-pages

--- rpm/rpm-install.sh
+++ rpm/rpm-install.sh
@@ -1,8 +1,17 @@
 # This is a replacement for the default disttools RPM build method
 # which gets the file lists right, including the byte-compiled files.
+#
+# We also process our man-pages here.
+
 python setup.py install --root=$RPM_BUILD_ROOT
+
+for manpage in $(cd doc && echo *.1); do
+  mkdir -m 755 -p $RPM_BUILD_ROOT/usr/share/man/man1/
+  install -v -m 644 doc/$manpage $RPM_BUILD_ROOT/usr/share/man/man1/
+  gzip --verbose $RPM_BUILD_ROOT/usr/share/man/man1/$manpage
+done
+
 find $RPM_BUILD_ROOT -type f \
   |sed -e "s|^$RPM_BUILD_ROOT/*|/|" \
        -e 's|/[^/]*$||' \
   |uniq >INSTALLED_FILES
-cat INSTALLED_FILES
