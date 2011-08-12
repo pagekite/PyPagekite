ca2db9f055d2a080145afb3b9ecf4f09a187d46b
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-08-12 16:03:53 +0000

    Another minor version bump

--- pagekite/__init__.py
+++ pagekite/__init__.py
@@ -94,7 +94,7 @@
 ###############################################################################
 #
 PROTOVER = '0.8'
-APPVER = '0.4.4e'
+APPVER = '0.4.4f'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 LICENSE_URL = 'http://www.gnu.org/licenses/agpl.html'
