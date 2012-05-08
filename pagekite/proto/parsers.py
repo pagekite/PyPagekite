7cf48191dd30706f2a6a40e9498fe0a0ed255cb7
Bjarni R. Einarsson <bre@beanstalks-project.net>
2012-05-08 23:30:03 +0000

    Boilerplate

--- pagekite/proto/parsers.py
+++ pagekite/proto/parsers.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python -u
+"""
+Protocol parsers for classifying incoming network connections.
+"""
+##############################################################################
+LICENSE = """\
+This file is part of pagekite.py.
+Copyright 2010-2012, the Beanstalks Project ehf. and Bjarni Runar Einarsson
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the  GNU  Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,  but  WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see: <http://www.gnu.org/licenses/>
+"""
+##############################################################################
 from pagekite.compat import *
 from pagekite.common import *
 import pagekite.logging as logging
