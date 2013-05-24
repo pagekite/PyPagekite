c1c6997cde4cabbc2aae101fb56e9ac69c646026
Bjarni R. Einarsson <bre@beanstalks-project.net>
2013-05-24 14:27:48 +0000

    Preparing v0.5.6b

--- pagekite/common.py
+++ pagekite/common.py
@@ -24,7 +24,7 @@ import random
 import time
 
 PROTOVER = '0.8'
-APPVER = '0.5.6a+github'
+APPVER = '0.5.6b'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 LICENSE_URL = 'http://www.gnu.org/licenses/agpl.html'
