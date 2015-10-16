1c475fb92a1fcd225867c4ff074d788c7820cb84
Bjarni R. Einarsson <bre@beanstalks-project.net>
2015-10-16 11:53:36 +0100

    Preparing release 0.5.8a, fixed .deb dependencies

--- pagekite/common.py
+++ pagekite/common.py
@@ -24,7 +24,7 @@ import random
 import time
 
 PROTOVER = '0.8'
-APPVER = '0.5.7b+github'
+APPVER = '0.5.8a'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'http://pagekite.net/'
 LICENSE_URL = 'http://www.gnu.org/licenses/agpl.html'
