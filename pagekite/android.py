da270c1bc58d326ef364f6c7118ec0fc1dfeb359
Bjarni R. Einarsson <bre@beanstalks-project.net>
2017-09-16 18:31:07 +0200

    It is 2017

--- pagekite/android.py
+++ pagekite/android.py
@@ -4,7 +4,7 @@ This is the main function for the Android version of PageKite.
 #############################################################################
 LICENSE = """\
 This file is part of pagekite.py.
-Copyright 2010-2016, the Beanstalks Project ehf. and Bjarni Runar Einarsson
+Copyright 2010-2017, the Beanstalks Project ehf. and Bjarni Runar Einarsson
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the  GNU  Affero General Public License as published by the Free
--- pagekite/proto/conns.py
+++ pagekite/proto/conns.py
@@ -5,7 +5,7 @@ incoming or outgoing network connections.
 ##############################################################################
 LICENSE = """\
 This file is part of pagekite.py.
-Copyright 2010-2016, the Beanstalks Project ehf. and Bjarni Runar Einarsson
+Copyright 2010-2017, the Beanstalks Project ehf. and Bjarni Runar Einarsson
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the  GNU  Affero General Public License as published by the Free
