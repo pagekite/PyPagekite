f1cd0a2ecf5078114c11e7bc433b90420338f36b
Bjarni R. Einarsson <bre@beanstalks-project.net>
2020-07-25 15:30:40 +0000

    Call this v1.0.1.200725

--- pagekite/common.py
+++ pagekite/common.py
@@ -24,7 +24,7 @@ import random
 import time
 
 PROTOVER = '0.8'
-APPVER = '1.0.1.200603'
+APPVER = '1.0.1.200725'
 AUTHOR = 'Bjarni Runar Einarsson, http://bre.klaki.net/'
 WWWHOME = 'https://pagekite.net/'
 LICENSE_URL = 'http://www.gnu.org/licenses/agpl.html'
