ae4fdeb5c276698a26ad0b8b6b9d0393774413bf
Bjarni R. Einarsson <bre@beanstalks-project.net>
2025-04-24 12:13:40 +0000

    Tweak pagekite-deb.sh to preserve "pagekite" as the process name

--- scripts/pagekite-deb.sh
+++ scripts/pagekite-deb.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+#
+# Figure out which version of pagekite (Python 2 or 3) is installed,
+# and run that. Prefer Python 3 if available.
+#
+"""true"
 if [ -e /usr/lib/python3/dist-packages/pagekite/__main__.py ]; then
-    exec python3 -m pagekite "$@"
+    exec python3 "$0" "$@"
 else
-    exec python -m pagekite "$@"
+    exec python "$0" "$@"
 fi
+"""
+from pagekite.__main__ import main
+main()
